6ded684779d07a9f4effa1b71850a8d97b4a4d17
Farah Amri <farah.amri@student.ehb.be>
2021-05-03 17:31:33 +0200

    data fetchen en schrijven naar mongoDB

---
+++ app.js
@@ -0,0 +1,72 @@
+"use strict";
+
+//SERVER SET-UP
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const cors = require('cors');
+const path = require('path');
+
+
+//MONGO DB SETUP
+const {
+    MongoClient,
+    ObjectID
+} = require('mongodb');
+const uri = "mongodb+srv://admin:admin@cluster0.lxqft.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+const databaseName = "data";
+let db, collection;
+
+//MIDDLEWARE
+app.use(cors());
+app.use(express.json({limit: '200mb'}));
+app.use(express.urlencoded({limit: '200mb', extended: true}));
+
+//ROUTER
+const apiRouter = express.Router();
+
+//ROOT
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+apiRouter.route('/data')
+    .post(async (req, res) => {
+        collection = db.collection("villoData");
+        collection.insertMany(req.body)
+            .then(result => {
+                console.log(result);
+                res.send("data is saved");
+            })
+            .catch(error => console.error(error));
+    })
+    .get((req, res) => {
+        collection = db.collection("villoData");
+        const {
+            query
+        } = req;
+        collection.find(query).toArray((err, result) => {
+            if (err) {
+                return res.send(err);
+            }
+            res.json(result);
+        });
+    });
+
+app.use('/api', apiRouter);
+
+//MONGO DB CONNECTION
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+    client.connect(err => {
+        if (err) {
+            throw err;
+        }
+        db = client.db(databaseName);
+        console.log("connected");
+    });
+});
---
+++ data.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const url = "https://api.jcdecaux.com/vls/v3/stations?contract=Bruxelles&apiKey=c5747f5adf36d81ba83846a75cc1d2d4b4116ab3";
+const apiUrl = "http://localhost:3000/api/data";
+
+async function getData(){
+    console.log("fetching...");
+    const response = await fetch(url);
+    const json = await response.json();
+    //console.log(json.mainStands);
+    renderData(json);
+    saveData(json);
+}
+
+//getData();
+
+setInterval(getData, 1800000);
+
+async function saveData(data){
+    let request = await fetch(apiUrl, {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+      return await request.json(); 
+}
+
+function renderData(json){
+    console.log(json);
+    let placeholder = document.getElementById('data');
+    let htmlString = "";
+
+    htmlString += JSON.stringify(json);
+
+    placeholder.insertAdjacentHTML("afterbegin", htmlString);
+}
+
+
+
+
+
+
