478544c153428fa4f7eaa33bcb9b3c1069335b18
Farah Amri <farah.amri@student.ehb.be>
2021-05-31 13:18:43 +0200

    added leaflet.js

--- front/index.js
+++ front/index.js
@@ -1,15 +1,18 @@
 "use strict";
 
 let model;
+const mymap = L.map('mapid').setView([50.8465573, 4.351697], 10);
+const marker = L.marker([0,0]).addTo(mymap);
 
 submitForm();
 
-function submitForm(){
+function submitForm() {
     let form = document.getElementById("form");
 
     form.addEventListener('submit', (event) => {
         event.preventDefault();
         setup();
+        //getLatAndLong();
     });
 }
 
@@ -17,24 +20,9 @@ function submitForm(){
 function setup() {
     model = ml5.neuralNetwork({
         dataUrl: 'data.json',
-        inputs: ['number','day','hour', 'minutes'],
+        inputs: ['number', 'day', 'hour', 'minutes'],
         outputs: ['bikes'],
         task: 'regression',
-        layers: [
-            {
-              type: 'dense',
-              units: 16,
-              activation: 'relu'
-            },
-            {
-              type: 'dense',
-              units: 16,
-              activation: 'sigmoid'
-            },
-            {
-              type: 'dense',
-              activation: 'sigmoid'
-            }],
         debug: true,
         learningRate: 0.0001
     }, modelLoaded);
@@ -44,8 +32,8 @@ function modelLoaded() {
     console.log('loaded');
     model.normalizeData();
     model.train({
-         batchSize: 50,
-         epochs: 10
+        batchSize: 50,
+        epochs: 10
     }, whileTraining, doneTraining);
 }
 
@@ -68,23 +56,27 @@ function doneTraining() {
     let minutes = parseInt(array[1]);
     console.log(minutes);
     console.log('done!');
-    
+
     //predict
 
-    model.predict([number, day, hour, minutes], function(err, result){
-        if(err){
+    model.predict([number, day, hour, minutes], function (err, result) {
+        if (err) {
             console.error(err);
         }
         let showOnHtml = document.getElementById('result');
 
-        result.map(el => {showOnHtml.innerText = el.bikes;});
+
+        result.map(el => {
+            showOnHtml.innerText = el.bikes;
+            getLatAndLong(el.bikes);
+        });
 
         console.log(result);
     });
 }
 
 
-async function fetchStations(){
+async function fetchStations() {
     let dropdown = document.getElementById("mystation");
     dropdown.length = 0;
 
@@ -92,14 +84,53 @@ async function fetchStations(){
 
     const response = await fetch(url);
     const stations = await response.json();
-    stations.map((d) =>{
+    stations.map((d) => {
         let option;
         option = document.createElement('option');
         option.text = d.name;
         option.value = d.number;
-       
+
         dropdown.add(option);
     });
 }
 
 fetchStations();
+
+function generateMap() {
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZmFyYWhhbXJpIiwiYSI6ImNrcGNnZGhtbDAwN3AydXBobG84cnUyemIifQ.OIzonTyh0WExSBezW9gslg', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1IjoiZmFyYWhhbXJpIiwiYSI6ImNrcGNnZGhtbDAwN3AydXBobG84cnUyemIifQ.OIzonTyh0WExSBezW9gslg'
+    }).addTo(mymap);
+}
+
+async function getLatAndLong(bikes){
+    let url = 'https://api.jcdecaux.com/vls/v3/stations?contract=Bruxelles&apiKey=c5747f5adf36d81ba83846a75cc1d2d4b4116ab3';
+    let latitude, longitude, number;
+    let value = Number(document.getElementById("mystation").value);
+
+    const response = await fetch(url);
+    const data = await response.json();
+    
+    data.map((d) => {
+        number = d.number;
+        latitude = d.position.latitude;
+        longitude = d.position.longitude;
+
+        if(value === number){
+            marker.setLatLng([latitude,longitude]).addTo(mymap);
+            marker.bindPopup(`er zullen ${bikes} fietsen beschikbaar zijn`).openPopup();
+        }    
+
+        return {
+            num: number,
+            lat: latitude,
+            long: longitude
+        };
+    });
+}
+
+generateMap();
